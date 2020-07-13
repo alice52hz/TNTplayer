ddb0a768a61d4a6376ab348a1cb542201f40fcfb
alice52hz <tech_hubing@163.com>
2020-07-13 21:45:53 +0800

    feat: set entrance of TNTplayer

--- src/tnt/index.js
+++ src/tnt/index.js
@@ -1,7 +1,44 @@
 import Player from '@/core/'
+import controls from '@/plugin/controls/'
+
+//  Methods to be proxied to TNTplayer
+const methodToProxyOnTNT = [
+    'on',
+    'once',
+    'isFullscreen',
+    'setFullScreen',
+    'exitFullscren',
+    'set',
+    'play',
+    'pause',
+    'load',
+    'seekTo'
+]
 
 export default class TNTplayer {
     constructor(opts) {
-        this.opts = opts
+        const ctrls = controls(opts)
+        const plugins = [ctrls]
+        const coreOpts = {
+            ...opts,
+            plugins
+        }
+        this.player = new Player(coreOpts)
+
+        //  proxy methods of core player to TNTplayer
+        methodToProxyOnTNT.forEach(method => {
+            this[method] = this.player[method].bind(this.player)
+        })
+
+        //  Proxy plugin's methods to TNTplayer instance
+        //  Public api method begin with $, they will be proxy to TNTplayer
+        plugins.forEach(plugin => {
+            const methods = plugin.pluginOpts.methods
+            for(const method in methods) {
+                if(method.charAt(0) == '$'){
+                    this[method] = methods[method].bind(plugin)
+                }
+            }
+        })
     }
 }
